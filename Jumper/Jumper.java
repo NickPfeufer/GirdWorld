2223d0e8ed13bccf388e50913887681b8221b605
apcs <apcs@mvhs-imac04u528.dcsdk12.org>
2014-01-16 08:58:09 -0700

    jumper constructer

---
+++ Jumper/Jumper.java
@@ -0,0 +1,23 @@
+import info.gridworld.actor.Bug;
+
+class Jumper extends Bug {
+	private int jumpsTaken = 0;
+	private int jumps = 0;
+
+	public Jumper(){
+		Jumper(1);
+	}
+
+	public Jumper(int jumps){
+		this.jumps = jumps;
+	}
+
+	public void act(){
+		
+	}
+
+
+
+
+
+}
