d6ad7ba12143dfcafabf44bb679c072dee5786c8
apcs <apcs@mvhs-imac04u528.dcsdk12.org>
2014-01-16 09:07:57 -0700

    act

--- Jumper/Jumper.java
+++ Jumper/Jumper.java
@@ -3,6 +3,7 @@ import info.gridworld.actor.Bug;
 class Jumper extends Bug {
 	private int jumpsTaken = 0;
 	private int jumps = 0;
+	private Location spot;
 
 	public Jumper(){
 		Jumper(1);
@@ -10,10 +11,21 @@ class Jumper extends Bug {
 
 	public Jumper(int jumps){
 		this.jumps = jumps;
+		self.spot = super.getLocation();
 	}
 
 	public void act(){
-		
+		if (jumpsTaken<jumps) {
+			jump();
+			jumpsTaken++;
+		}else {
+			turn();
+			jumpsTaken = 0;
+		}
+	}
+
+	public void jump(){
+
 	}
 
 
