f04982dae12574ce2a7f8471d111354334610150
apcs <apcs@mvhs-imac04u528.dcsdk12.org>
2014-01-23 08:31:59 -0700

    Test LVL

--- Jumper/Jumper.java
+++ Jumper/Jumper.java
@@ -1,31 +1,50 @@
 import info.gridworld.actor.Bug;
+import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
+import info.gridworld.actor.Actor;
 
 class Jumper extends Bug {
 	private int jumpsTaken = 0;
 	private int jumps = 0;
+	private Grid<Actor> grid;
 
-	public Jumper(){
-		Jumper(1);
-	}
+
 
 	public Jumper(int jumps){
+		// for custom jump limits
 		this.jumps = jumps;
 	}
 
-	public void jump(){
-		Location current = super.getLocation();
-		Location nextLocation = current.adjacentLocation(getDirection());
-		Location afterNext = nextLocation.adjacentLocation(getDirection());
+	public Jumper(){
+		// for lazy progamers
+		this(1);
+	}
 
-		if(canJump(afterNext)){
+	public void jump(){
+		// find location two squares ahead of the bug for testing and moveing
+		Location current = getLocation();
+		Location nextLocation = current.getAdjacentLocation(getDirection());
+		Location afterNext = nextLocation.getAdjacentLocation(getDirection());
+		// then we get the grid
+		this.grid = getGrid();
+		// test jumping/ jump limit
+		if(canJump(afterNext) && jumpsTaken < jumps){
+			// leaves no flowers when we use move to
 			moveTo(afterNext);
+			// count jumps until limit
+			jumpsTaken++;
 		}else{
+			// if it can't jump, it turns
 			turn();
+			// when ever it turns reset jumps taken
+			jumpsTaken = 0;
 		}	
 	}
 	public boolean canJump(Location xy){
-		if (xy.isValid()) {
-			if (get(xy) != null) {
+		// if the point is not on the grid, it dosen't matter
+		if (grid.isValid(xy)) {
+			// check for other actors
+			if (grid.get(xy) != null) {
 				return false;
 			}else{
 				return true;
@@ -35,7 +54,7 @@ class Jumper extends Bug {
 	}
 
 	public void act(){
-		
+		jump();
 	}
 
 
--- Jumper/JumperRunner.java
+++ Jumper/JumperRunner.java
@@ -1,9 +1,40 @@
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.Rock;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Bug;
+
+import java.awt.Color;
+
 public class JumperRunner{ 
     public static void main(String[] args)
     {
         ActorWorld world = new ActorWorld();
-	Jumper joseph = new Jumper(2);
-	world.add(new Location(4,4), zedd);
+        // add jumper
+		Jumper joseph = new Jumper(6);
+		joseph.setColor(Color.BLUE);
+		world.add(new Location(5, 5), joseph);
+		//build test track
+		world.add(new Location(4,5), new Rock());
+		world.add(new Location(1,5), new Rock());
+		world.add(new Location(3,9), new Flower());
+		world.add(new Location(8,9), new Flower());
+		world.add(new Location(6,2), new Rock());
+		world.add(new Location(6,3), new Rock());
+		world.add(new Location(6,4), new Rock());
+		world.add(new Location(7,2), new Rock());
+		world.add(new Location(7,3), new Bug());
+		world.add(new Location(7,4), new Rock());
+		world.add(new Location(8,2), new Rock());
+		world.add(new Location(8,3), new Rock());
+		world.add(new Location(8,4), new Rock());
+		world.add(new Location(5,3), new Rock());
+		world.add(new Location(3,3), new Rock());
+		// if you let it run for some time, Joseph will get in greens way
+		// and green will turn
+		Jumper green = new Jumper(5);
+		green.setColor(Color.green);
+		world.add(new Location(9,9), green);				
         world.show();
     }
 }
